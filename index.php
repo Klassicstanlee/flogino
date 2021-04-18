1582b86e3ba8d6f317bcea5914b21286dc5364cf
Stanlee Olibe <stanleeolibe@gmail.com>
2021-04-19 00:30:38 +0100

    modified file

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='./main.css'>
+    <script src='./index.html'></script>
+</head>
+<body>
+    <div class="x-container">
+        <header class="page_header">
+            <nav class="page_nav">
+                <div class="brand">
+                    Flogins
+                </div>
+
+                <div class="log_in">
+                    Already have an acoount? <a href="./"> Log in</a>
+                </div>
+                <a href="./" class="btn"> Log in</a>
+                
+            </nav>
+
+            <div class="form_container">
+                <h1>Get your free account</h1>
+
+                <form action="" id="form">
+                    <div class="input_container">
+                        <input type="button" value="Continue with Gogogle" />
+                    </div>
+
+                    <div class="input_container">
+                        <input type="button" value="Continue with Apple" />
+                    </div>
+                    
+                    <div class="input_container">
+                        <hr />
+                    </div>
+
+                    <div class="input_container">
+                        <input type="email" name="email" id="email" placeholder="Email" autofocus required />
+                    </div>
+
+                </form>         
+            </div>
+        </header>
+    </div>
+</body>
+</html>
